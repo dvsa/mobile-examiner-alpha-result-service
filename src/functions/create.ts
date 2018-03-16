7f5371f0da46eda8288913e84e66aad81291f33b
Kamil Zielinski <K.Zielinski@kainos.com>
2018-03-16 16:58:30 +0000

    added type checking -- locally we got body as string, on AWS we got parsed obj

--- src/functions/create.ts
+++ src/functions/create.ts
@@ -7,8 +7,19 @@ import { TestResultService } from '../services/testResult';
 const testResultService = new TestResultService(doc, process.env.DYNAMODB_TABLE);
 
 export default (event: any, context: Context, callback: Callback) => {
-	const data = JSON.parse(event.body);
-	const testResult: ITestResult = (({ _candidateId, faults }) => ({ _candidateId, faults }))(data);
+	let body: any;
+	
+	if (event.body && typeof (event.body) === 'object') {
+		body = event.body
+	} else {
+		try {
+			body = JSON.parse(event.body);
+		} catch (e) {
+			console.error(`Couldn\'t parse body ${e}`)
+		}
+	}
+
+	const testResult: ITestResult = (({ _candidateId, faults }) => ({ _candidateId, faults }))(body);
 
 	testResultService.create(testResult, callback);
 };
