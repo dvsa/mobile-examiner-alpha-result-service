1866be074d6a2dc65d1bd2f4312e6820f5962652
Kamil Zielinski <K.Zielinski@kainos.com>
2018-03-19 13:52:01 +0000

    modified create response function

--- src/utils/createResponse.ts
+++ src/utils/createResponse.ts
@@ -1,10 +1,13 @@
-export default (body: any, statusCode = 200) => {
-	const response = {
-		statusCode,
-		headers: {
-			'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-		},
-		body: JSON.stringify(body),
+import { IResponse } from "../interfaces/interface";
+
+export default (body: any, statusCode = 200, reqHeaders: { [id: string]: string; } = {}): IResponse => {
+	const accessControlAllowOriginHeader = {
+		'Access-Control-Allow-Origin': '*' // Required for CORS support to work
 	};
-	return response;
+
+	return {
+		statusCode,
+		headers: { ...accessControlAllowOriginHeader, ...reqHeaders},
+		body: (body === null)? null :JSON.stringify(body),
+	};
 };
