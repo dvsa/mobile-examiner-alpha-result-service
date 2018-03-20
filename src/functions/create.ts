077da61bd9642969a892df2a0aef1578b12ec3d6
Kamil Zielinski <K.Zielinski@kainos.com>
2018-03-20 11:09:05 +0000

    added event type

--- src/functions/create.ts
+++ src/functions/create.ts
@@ -1,11 +1,11 @@
 import { doc } from 'serverless-dynamodb-client';
-import { Context, Callback } from 'aws-lambda';
+import { Context, Callback, APIGatewayEvent } from 'aws-lambda';
 
 import { ITestResult } from '../interfaces/itest-result';
 import TestResultService from '../services/test-result';
 
 const testResultService = new TestResultService(doc, process.env.DYNAMODB_TABLE);
 
-export default (event: any, context: Context, callback: Callback) => {
+export default (event: APIGatewayEvent, context: Context, callback: Callback) => {
 	testResultService.create(event.body, callback);
 };
