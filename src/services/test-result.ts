8e13fd18135b62cfc739698b802844de25291fb1
fredski02 <frederico02@hotmail.com>
2018-04-23 11:11:15 +0100

    Saving faults as a serialized string works. Unit tests have also been updated to test for strings instead of arrays

--- src/services/test-result.ts
+++ src/services/test-result.ts
@@ -1,85 +1,93 @@
-import { ITestResult } from '../interfaces/itest-result';
-import { Callback } from 'aws-lambda';
-import createResponse from '../utils/create-response';
-import { DynamoDB, AWSError } from 'aws-sdk';
-import * as UUID from 'uuid'
-import { IResponse } from '../interfaces/iresponse';
+import { ITestResult } from "../interfaces/itest-result";
+import { Callback } from "aws-lambda";
+import createResponse from "../utils/create-response";
+import { DynamoDB, AWSError } from "aws-sdk";
+import * as UUID from "uuid";
+import { IResponse } from "../interfaces/iresponse";
 
 export default class TestResultService {
+  constructor(private db: DynamoDB.DocumentClient, private tableName: string) {}
 
-	constructor(private db: DynamoDB.DocumentClient, private tableName: string) { }
-	
-	create(requestBody: any, callback: Callback) {
-		const testResultData: ITestResult = this.extractTestResult(requestBody);
-		const validationErrorResponse = this.validateTestResult(testResultData);
-		
-		if (validationErrorResponse) {
-			return callback(null, validationErrorResponse)
-		}
+  create(requestBody: any, callback: Callback) {
+    const testResultData: ITestResult = this.extractTestResult(requestBody);
+    const validationErrorResponse = this.validateTestResult(testResultData);
 
-		const id = UUID.v1()
-		const params = {
-			TableName: this.tableName,
-			Item: { ...testResultData, id }
-		};
-		
-		console.log(params);
+    if (validationErrorResponse) {
+      return callback(null, validationErrorResponse);
+    }
 
-		this.db.put(params, (err: AWSError) => {
-			if (err) {
-				const error = createResponse({
-					message: 'Error!',
-					error: err
-				}, 500);
+    const id = UUID.v1();
+    const params = {
+      TableName: this.tableName,
+      Item: { ...testResultData, id }
+    };
 
-				callback(null, error);
-			} else {
-				const statusCode = 201;
-				const headers = {
-					'Location': `/test-result/${id}`,
-					'Access-Control-Expose-Headers': 'Location'
-				}
+    this.db.put(params, (err: AWSError) => {
+      if (err) {
+        const error = createResponse(
+          {
+            message: "Error!",
+            error: err
+          },
+          500
+        );
 
-				callback(null, createResponse(null, statusCode, headers));
-			}
-		});
-	}
+        callback(null, error);
+      } else {
+        const statusCode = 201;
+        const headers = {
+          Location: `/test-result/${id}`,
+          "Access-Control-Expose-Headers": "Location"
+        };
 
-	private validateTestResult(testResultData: ITestResult): IResponse | null {
-		if (!testResultData.candidateId || typeof (testResultData.candidateId) !== 'string') {
-			return this.createMissingPropertyError('candidateId')
-		} else if (!testResultData.faults || typeof (testResultData.faults) !== 'string') {
-			return this.createMissingPropertyError('faults')
-		}
+        callback(null, createResponse(null, statusCode, headers));
+      }
+    });
+  }
 
-		return null;
-	}
+  private validateTestResult(testResultData: ITestResult): IResponse | null {
+    if (
+      !testResultData.candidateId ||
+      typeof testResultData.candidateId !== "string"
+    ) {
+      return this.createMissingPropertyError("candidateId");
+    } else if (
+      !testResultData.faults ||
+      typeof testResultData.faults !== "string"
+    ) {
+      return this.createMissingPropertyError("faults");
+    }
 
-	private createMissingPropertyError(propertyName): IResponse {
-		return createResponse({
-			title: 'Error',
-			message: `${propertyName} property is missing`,
-		}, 400);
-	}
+    return null;
+  }
 
-	private extractTestResult(object: any): ITestResult {
-		let body: any;
-		
-		if (object && typeof (object) === 'object') {
-			body = object
-		} else {
-			try {
-				body = JSON.parse(object);
-				// body.faults = JSON.parse(body.faults)
-			} catch (e) {
-				console.error(`Couldn\'t parse body ${e}`)
-			}
-		}
+  private createMissingPropertyError(propertyName): IResponse {
+    return createResponse(
+      {
+        title: "Error",
+        message: `${propertyName} property is missing`
+      },
+      400
+    );
+  }
 
-		const { candidateId, faults } = body;
-		return {
-			candidateId,
-			faults
-		}
-	}
+  private extractTestResult(object: any): ITestResult {
+    let body: any;
+
+    if (object && typeof object === "object") {
+      body = object;
+    } else {
+      try {
+        body = JSON.parse(object);
+      } catch (e) {
+        console.error(`Couldn\'t parse body ${e}`);
+      }
+    }
+
+    const { candidateId, faults } = body;
+    return {
+      candidateId,
+      faults
+    };
+  }
 }
